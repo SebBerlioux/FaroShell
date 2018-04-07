038a1295396181e3e3fadf89311f98c60a253da8
root <root@LAPTOP-A353ON3Q.localdomain>
2018-04-07 19:12:07 +0200

    maj mkdir.c

--- src/cmd/mkdir/mkdir.c
+++ src/cmd/mkdir/mkdir.c
@@ -10,12 +10,12 @@ void fmkdir(int argc, char const *argv[]){
   char *folderName[20];
 
   for(int i=0; i<argc; i++){
-    if(argv[i] == "-m"){ // -m gère les permissions du répertoire créé
-      folderMode = argv[i+1]; //les elements sont separés par un espace donc les valeurs de -m sont en i+1
+    if(argv[i] == (char*)("-m")){ // -m gère les permissions du répertoire créé
+      folderMode = (char*)argv[i+1]; //les elements sont separés par un espace donc les valeurs de -m sont en i+1
       i++; //on saute alors les chiffres après le -m
     }else{ // nom du fichier             /!\ on ne gère pas les cas d'erreurs ici
-      folderName = argv[i];
+      *folderName = (char*)argv[i];
     }
   }
-  mdkdir(folderName, folderMode);
+  mkdir(*folderName, *folderMode);
 }
