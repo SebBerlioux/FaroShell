492029bebe827c7fea9af0a6b2722daf718caa70
sebastien <sebastien.berlioux@etu.univ-nantes.fr>
2018-04-11 14:36:57 +0200

    modification cd

--- src/cmd/cd/cd.c
+++ src/cmd/cd/cd.c
@@ -1,8 +1,6 @@
 #include "cd.h"
 
 void fcd(int argc, char const *argv[]){
-  printf("hello\n");
-
   if(argc == 1){
     struct passwd *pw = getpwuid(getuid()); // on recupère l'ID de l'utilisateur courant
     const char *homedir = pw->pw_dir; // On recupere ensuite le mot de passe qui contient le dossier personnel "HOME" de l'utilisateur
--- src/cmd/cd/cd.h
+++ src/cmd/cd/cd.h
@@ -1,11 +1,9 @@
 #ifndef _CD_H
 	#define _CD_H
 
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
-#include <string.h>
 #include <stdlib.h>
 
 void fcd(int argc, char const *argv[]);
