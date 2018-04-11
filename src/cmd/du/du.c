7b1a2f3a023fdbf5e7fcfb6c52c54c731e774141
sebastien <sebastien.berlioux@etu.univ-nantes.fr>
2018-04-11 16:21:26 +0200

    Ajout commande du

---
+++ src/cmd/du/du.c
@@ -0,0 +1,31 @@
+#include "du.h"
+
+void fdu(int argc, char const *argv[]){
+
+  struct stat s;
+
+  if(argc == 1){ // dossier HOME
+    // on recupere le directoire HOME
+    struct passwd *pw = getpwuid(getuid());
+    const char *homedir = pw->pw_dir;
+    stat(homedir,&s); // on stocke dans s les données du dossier homedir
+    printf("%li\t\t%s\n", s.st_size, homedir);
+  }else{
+
+    DIR *dir;
+    struct dirent *subdir = NULL;
+
+    dir = opendir(argv[1]);
+    if(dir == NULL){
+      printf("Le dossier %s n'existe pas !", argv[1]);
+      exit(EXIT_FAILURE);
+    }
+    // ici, le dossier existe
+    while((subdir = readdir(dir)) != NULL){ // on parcours les sous dossiers subdir du dossier dir
+      stat(subdir->d_name,&s);
+      printf("%ld\t\t%s\n", s.st_size, subdir->d_name);
+    }
+    closedir(dir);
+  }
+
+}
---
+++ src/cmd/du/du.h
@@ -0,0 +1,15 @@
+#ifndef _DU_H
+	#define _DU_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include "../pwd/pwd.c"
+
+void fdu(int argc, char const *argv[]);
+
+#endif
