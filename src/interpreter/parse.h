a954d6ccaaa6168dd29056579af3abce1334d0d9
Bastien JEAN <bastien.jean@etu.univ-nantes.fr>
2018-04-15 12:11:41 +0200

    Ajout des appels des différentes fonctions dans parse.h

--- src/interpreter/parse.h
+++ src/interpreter/parse.h
@@ -7,6 +7,13 @@
 
 #include "liste.h"
 #include "../cmd/echo/echo.h"
+#include "../cmd/cat/cat.h"
+#include "../cmd/pwd/pwd.h"
+#include "../cmd/cd/cd.h"
+#include "../cmd/rm/rm.h"
+#include "../cmd/mv/mv.h"
+#include "../cmd/mkdir/mkdir.h"
+#include "../cmd/ls/ls.h"
 
 #define NOMBRE_ARGUMENT 10
 
