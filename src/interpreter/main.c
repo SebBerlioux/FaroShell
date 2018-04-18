13741185b86be78a68ad66e6b61cfd2a1e1d24cc
Simon BESSENAY <simon.bessenay@etu.univ-nantes.fr>
2018-04-18 15:53:55 +0200

    Ajout du chemin courant et du username au prompt

--- src/interpreter/main.c
+++ src/interpreter/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <pwd.h>
 
 #include "liste.h"
 #include "lecture.h"
@@ -13,18 +14,32 @@
 #define ANSI_COLOR_YELLOW  "\x1b[93m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+const char *getUserName()
+{
+  uid_t uid = geteuid();
+  struct passwd *pwd = getpwuid(uid);
+  if (pwd)
+  {
+    return pwd->pw_name;
+  }
+
+  return "";
+}
+
 int main(void)
 {
 	printf("\033[H\033[2J");	// efface la console
 
+	char tampon[100];
+
 	char *commandLine;	// ligne de commande rentree
 	SList *parametresList;	// liste des parametres (dont nom de commande)
-
+	
 	do
 	{
 		parametresList = CreateList();	// init liste
-		printf("[" ANSI_COLOR_YELLOW "User" ANSI_COLOR_CYAN "@%s"
-			ANSI_COLOR_RESET "]\n", SHELLNAME);
+		printf(ANSI_COLOR_YELLOW "%s" ANSI_COLOR_RESET":" ANSI_COLOR_CYAN "%s"
+			ANSI_COLOR_RESET, getUserName(), getcwd(tampon, 100));
 		printf("$ ");	// prompt
 		commandLine = lireCommande();	// lecture d'une commande entiere
 		if (strcmp(commandLine, ""))
