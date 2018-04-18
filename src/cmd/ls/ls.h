f320d53051fdfed67ae3bbab7ed1a55ee170cc5a
Arthur Millet <arthur.millet@etu.univ-nantes.fr>
2018-04-18 23:30:54 +0200

    modif define ls

--- src/cmd/ls/ls.h
+++ src/cmd/ls/ls.h
@@ -15,51 +15,6 @@
 #include <limits.h>
 
 
-#define	S_IRUSR	0000400			/* R for owner */
-#define	S_IWUSR	0000200			/* W for owner */
-#define	S_IXUSR	0000100			/* X for owner */
-#define	S_IRGRP	0000040			/* R for group */
-#define	S_IWGRP	0000020			/* W for group */
-#define	S_IXGRP	0000010			/* X for group */
-#define	S_IROTH	0000004			/* R for other */
-#define	S_IWOTH	0000002			/* W for other */
-#define	S_IXOTH	0000001			/* X for other */
-#define	S_IFIFO	 0010000		/* named pipe (fifo) */
-#define	S_IFCHR	 0020000		/* character special */
-#define	S_IFDIR	 0040000		/* directory */
-#define	S_IFBLK	 0060000		/* block special */
-#define	S_IFLNK	 0120000		/* symbolic link */
-#define	S_ISSOCK(m)	((m & 0170000) == 0140000)	/* socket */
-#define	S_IFSOCK 0140000		/* socket */
-#define	S_ISDIR(m)	((m & 0170000) == 0040000)	/* directory */
-#define	S_ISCHR(m)	((m & 0170000) == 0020000)	/* char special */
-#define	S_ISBLK(m)	((m & 0170000) == 0060000)	/* block special */
-#define	S_ISFIFO(m)	((m & 0170000) == 0010000)	/* fifo */
-#define	S_ISLNK(m)	((m & 0170000) == 0120000)	/* symbolic link */
-#define	S_IFREG	 0100000		/* regular */
-
-
-
-
-#include <unistd.h>
-
-int getopt(int argc, char * const argv[],
-           const char *optstring);
-
-extern char *optarg;
-extern int optind, opterr, optopt;
-
-#define _GNU_SOURCE
-#include <getopt.h>
-
-int getopt_long (int argc, char * const argv[],
-                 const char *optstring,
-                 const struct option *longopts, int *longindex);
-
-int getopt_long_only (int argc, char * const argv[],
-                      const char *optstring,
-                      const struct option *longopts, int *longindex);
-
 
 
 
