fdf553c091fcca2226cd8ab54af1dbbb6a9c52e7
Arthur Millet <arthur.millet@etu.univ-nantes.fr>
2018-04-23 17:12:50 +0200

    sup

--- src/cmd/cp/cp.c
+++ src/cmd/cp/cp.c
@@ -1,84 +1,6 @@
 #include <stdio.h>
 #include "cp.h"
 
-//Copie d'un dossier dans un autre dossier
-int cp_avanced(const char *Source_ID,const char *Destination_ID){
-
-    DIR *source;           	            // le dossier (et son contenu) à deplacer
-    DIR *destination;           	    // sa destination
-    struct dirent *sourceEntry;     	// information du dossier source
-    struct stat file_stat;    	        // nformation du fichier source
-    char buffer[246] = {0};	        // chemin du fichier contenu dans le dossier source
-
-    //Cas avec erreur
-    if ((source=opendir(Source_ID))==NULL){
-        printf("Impossible d'ouvrir le repertoire %s\n",Source_ID);
-        return -1;
-    }
-
-    if ((destination=opendir(Destination_ID))==NULL){
-        printf("Le repertoire existe pas %s\n",Destination_ID);
-        return -1;
-    }
-
-    // FDéplacement du fichier contenu dans le repertoire
-    while ((sourceEntry=readdir(source))!=NULL) {
-
-        // On ignore si c'est "." ou ".."
-        if (strcmp(sourceEntry->d_name,".")==0 || strcmp(sourceEntry->d_name,"..")==0) {
-            continue;
-        }
-
-        // Création du chemin
-        snprintf(buffer, 246, "%s/%s", Source_ID, sourceEntry->d_name);
-        char path_Src[246], path_Dest[246];
-        strcpy(path_Src,buffer);
-        strcpy(path_Dest,Destination_ID);
-
-        if(path_Src[strlen(path_Src)-1]!='/') strcat(path_Dest, "/");
-        strcat(path_Dest, sourceEntry->d_name);
-
-        // On prends les informations du fichier
-        stat(path_Src, &file_stat);
-
-        // Si c'est une cible sans répertoire c'est bon
-        if (S_ISREG(file_stat.st_mode)){
-
-            FILE *f_Src;
-            FILE *f_Dest;
-            char tmp[1024];
-            int i=0;
-
-            if((f_Src = fopen(path_Src, "r")) == NULL){
-                // Si le fichier s'ouvre pas
-                printf("Le fichier source %s semble introuvable dans le dossier\n", path_Src);
-                return 1;
-            }
-
-            if((f_Dest = fopen(path_Dest, "w")) == NULL){
-                // Si le dossier de destination n'existe pas
-                printf("Le fichier de destination : %s semble introuvable dans le dossier\n", path_Dest);
-                fclose(f_Src);
-                return 2;
-            }
-
-            while((i = fread(tmp, 1, 4096, f_Src)) != 0){
-                fwrite(tmp, 1, i, f_Dest);
-            }
-
-            fclose(f_Dest); //On ferme tout
-            fclose(f_Src);
-        }
-
-            //Si c'est encore un dossier on relancela fonction qui est RECURSIVE
-        else if ( S_ISDIR(file_stat.st_mode) ){
-            cp_avanced(path_Src,path_Dest); //On relance la fonction
-        }
-    }
-
-    return 0;
-}
-
 // Fonction qui sert  à  copier  des  fichiers  et eventuellement des répertoires.
 int fcp(int argc, char *argv[]){
 
@@ -153,6 +75,85 @@ int fcp(int argc, char *argv[]){
             return -1;
         }
     }
+    return 0;
+}
+
+
+//Copie d'un dossier dans un autre dossier (on veut copier un dossier en récurcif correspond à l'opt -r) Répertoire à Rpertoire
+int cp_avanced(const char *Source_ID,const char *Destination_ID){
+
+    DIR *source;           	            // le dossier (et son contenu) à deplacer
+    DIR *destination;           	    // sa destination
+    struct dirent *sourceEntry;     	// information du dossier source
+    struct stat file_stat;    	        // nformation du fichier source
+    char buffer[246] = {0};	        // chemin du fichier contenu dans le dossier source
+
+    //Cas avec erreur
+    if ((source=opendir(Source_ID))==NULL){
+        printf("Impossible d'ouvrir le repertoire %s\n",Source_ID);
+        return -1;
+    }
+
+    if ((destination=opendir(Destination_ID))==NULL){
+        printf("Le repertoire existe pas %s\n",Destination_ID);
+        return -1;
+    }
+
+    // FDéplacement du fichier contenu dans le repertoire
+    while ((sourceEntry=readdir(source))!=NULL) {
+
+        // On ignore si c'est "." ou ".."
+        if (strcmp(sourceEntry->d_name,".")==0 || strcmp(sourceEntry->d_name,"..")==0) {
+            continue;
+        }
+
+        // Création du chemin
+        snprintf(buffer, 246, "%s/%s", Source_ID, sourceEntry->d_name);
+        char path_Src[246], path_Dest[246];
+        strcpy(path_Src,buffer);
+        strcpy(path_Dest,Destination_ID);
+
+        if(path_Src[strlen(path_Src)-1]!='/') strcat(path_Dest, "/");
+        strcat(path_Dest, sourceEntry->d_name);
+
+        // On prends les informations du fichier
+        stat(path_Src, &file_stat);
+
+        // Si c'est une cible sans répertoire c'est bon
+        if (S_ISREG(file_stat.st_mode)){
+
+            FILE *f_Src;
+            FILE *f_Dest;
+            char tmp[1024];
+            int i=0;
+
+            if((f_Src = fopen(path_Src, "r")) == NULL){
+                // Si le fichier s'ouvre pas
+                printf("Le fichier source %s semble introuvable dans le dossier\n", path_Src);
+                return 1;
+            }
+
+            if((f_Dest = fopen(path_Dest, "w")) == NULL){
+                // Si le dossier de destination n'existe pas
+                printf("Le fichier de destination : %s semble introuvable dans le dossier\n", path_Dest);
+                fclose(f_Src);
+                return 2;
+            }
+
+            while((i = fread(tmp, 1, 4096, f_Src)) != 0){
+                fwrite(tmp, 1, i, f_Dest);
+            }
+
+            fclose(f_Dest); //On ferme tout
+            fclose(f_Src);
+        }
+
+            //Si c'est encore un dossier on relancela fonction qui est RECURSIVE
+        else if ( S_ISDIR(file_stat.st_mode) ){
+            cp_avanced(path_Src,path_Dest); //On relance la fonction
+        }
+    }
+
     return 0;
 }
 
